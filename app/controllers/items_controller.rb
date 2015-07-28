ba89be64e2e213c124365c2b7989b276cfc03463
Pam Willenz <pam@positek.net>
2015-07-28 15:32:48 -0400

    still working on items displaying

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,21 @@
 class ItemsController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @item = Item.new
+  end
+
   def create
     @user = User.find(params[:user_id])
-    @item = Item.new(item_params)
+    @items = @user.items
+    @item = current_user.items.build(item_params)
+    @item.user = @user
+    @new_item = Item.new
+
+    if @item.save
+      flash[:notice] = "Your item was saved."
+    else
+      flash[:error] = "There was an error saving your item."
+    end
   end
 
   
